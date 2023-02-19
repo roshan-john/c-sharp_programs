a9911a5a8fdd1b35906837d0d54a8d40be21f0e8
roshan-john <roshan1998john@gmail.com>
2023-02-19 22:36:36 +0530

    hello world

---
+++ task_01_hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
