7de6d3f6cc2eaf7c5297c5f9ed178f6419accb2f
roshan-john <roshan1998john@gmail.com>
2023-02-20 15:42:42 +0530

    swap numbers

---
+++ task_02_swap_numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MySpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            int secondNum = int.Parse(Console.ReadLine());
+            int temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+            Console.WriteLine("The numbers after swapping are "+firstNum+" and "+secondNum);
+        }
+    }
+}
